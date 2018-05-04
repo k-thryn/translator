0896ad01cd9fc8d7c97fca68bbc7fa212e6eb1fb
Kathryn Stavish <KStavish@rightpoint.com>
2018-05-04 17:04:54 -0400

    Added basic onclick methods to buttons

--- app/src/main/java/kstavish/translatorapp/MainActivity.kt
+++ app/src/main/java/kstavish/translatorapp/MainActivity.kt
@@ -2,11 +2,24 @@ package kstavish.translatorapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.translate_text_input
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Translate to yoda
+    fun translate(view: View) {
+        // Get text to be translated
+        val inputText = translate_text_input.text.toString();
+        // TODO: translate
+        val myToast = Toast.makeText(this, "Translation complete", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
