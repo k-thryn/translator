398632bbbb53e722446e84c9f8bcc50cefc6c7fe
Kathryn Stavish <KStavish@rightpoint.com>
2018-05-07 10:37:43 -0400

    Imperfect API integration

--- app/src/main/java/kstavish/translatorapp/MainActivity.kt
+++ app/src/main/java/kstavish/translatorapp/MainActivity.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.translate_text_input
+import kstavish.translatorapp.R.id.translate_text_input
+import org.json.JSONObject
+import java.net.URL
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +18,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    // Translate to yoda
-    fun translate(view: View) {
-        // Get text to be translated
-        val inputText = translate_text_input.text.toString();
-        // TODO: translate
-        val myToast = Toast.makeText(this, "Translation complete", Toast.LENGTH_SHORT)
-        myToast.show()
+    // Onclick function; translate to Yoda
+    fun translateYoda(view: View) {
+        val endpoint = "/yoda.json"
+        translate(view, endpoint)
+    }
+
+    // Onclick function; translate to Pirate
+    fun translatePirate(view: View) {
+        val endpoint = "/pirate.json"
+        translate(view, endpoint)
+    }
+
+    // Onclick function: translate to Pig Latin
+    fun translatePigLatin(view: View) {
+        val endpoint = "/piglatin.json"
+        translate(view, endpoint)
+    }
+
+    // Translate
+    private fun translate(view: View, endpoint: String) {
+        // Generate full url using root url and text
+        val base = "http://api.funtranslations.com/translate"
+        val text = translate_text_input.text.toString()
+        val url = "$base$endpoint?text=$text"
+
+        var translated: String? = ""
+
+        // Make call
+        Executors.newSingleThreadExecutor().execute({
+            val result = URL(url).readText()
+            val obj = JSONObject(result)
+            translated = obj.getJSONObject("contents")?.getString("translated")
+        })
+
+        val toast = Toast.makeText(this, translated, Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
